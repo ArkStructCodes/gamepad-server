164dbef68057cfc1c43749f8b20254d14b464b1a
ArkStructCodes <153435292+ArkStructCodes@users.noreply.github.com>
2025-05-09 13:26:53 +0530

    Allow the user to specify a port to use

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,43 @@ mod input;
 mod server;
 mod utils;
 
-use std::io::{ErrorKind, Result};
-use std::process::exit;
-use std::sync::mpsc::{Receiver, Sender, channel};
-use std::thread::spawn;
+use std::env;
+use std::io;
+use std::process;
+use std::sync::mpsc;
+use std::thread;
 
 use log::{error, warn};
 
 use crate::input::{Gamepad, GamepadInput};
 use crate::server::Server;
-use crate::utils::init_logger;
 
-fn start_server(port: u16, sender: Sender<[u8; 14]>) -> Result<()> {
-    let server = Server::new(port)?.listen()?;
-    server.recv_to(sender)
+fn get_port_from_args() -> Option<u16> {
+    env::args().skip(1).next()?.parse().ok()
 }
 
-fn recv_inputs(receiver: Receiver<[u8; 14]>) -> Result<()> {
+fn start_server(port: u16, tx: mpsc::Sender<[u8; 14]>) -> io::Result<()> {
+    let server = Server::new(port)?.listen()?;
+    server.recv_to(tx)
+}
+
+fn handle_server_errors(error: io::Error) {
+    match error.kind() {
+        io::ErrorKind::AddrNotAvailable |
+        io::ErrorKind::AddrInUse |
+        io::ErrorKind::Other => {
+            error!("FATAL: {error}");
+            let code = error.raw_os_error().unwrap_or(1);
+            process::exit(code);
+        }
+        io::ErrorKind::ConnectionAborted => warn!("client disconnected"),
+        _ => warn!("{error}"),
+    }
+}
+
+fn recv_inputs(rx: mpsc::Receiver<[u8; 14]>) -> io::Result<()> {
     let mut gamepad = Gamepad::new("Virtual Gamepad")?;
-    while let Ok(data) = receiver.recv() {
+    while let Ok(data) = rx.recv() {
         gamepad.emit(GamepadInput::try_from(&data)?)?;
     }
 
@@ -30,31 +48,28 @@ fn recv_inputs(receiver: Receiver<[u8; 14]>) -> Result<()> {
 }
 
 fn main() {
-    init_logger("info");
+    utils::init_logger("info");
+    let port = get_port_from_args().unwrap_or_else(|| {
+        warn!("no valid port provided, using the default port");
+        11096
+    });
+    let (tx, rx) = mpsc::channel();
 
-    let (sender, receiver) = channel();
-
-    spawn(move || {
+    thread::spawn(move || {
         loop {
-            let Err(e) = start_server(3000, sender.clone()) else {
+            let Err(e) = start_server(port, tx.clone()) else {
                 warn!("client timeout, aborting connection");
                 continue;
             };
-
-            match e.kind() {
-                ErrorKind::AddrNotAvailable | ErrorKind::Other => {
-                    error!("fatal: {}", e);
-                    exit(e.raw_os_error().unwrap_or(1));
-                },
-                ErrorKind::ConnectionAborted => warn!("client disconnected"),
-                _ => warn!("{}", e),
-            }
-
+            handle_server_errors(e); // will exit on fatal errors
             warn!("reloading server");
+            if port == 0 {
+                warn!("selecting a new port");
+            }
         }
     });
 
-    if let Err(e) = recv_inputs(receiver) {
-        error!("fatal: {}", e);
+    if let Err(e) = recv_inputs(rx) {
+        error!("FATAL: {e}");
     }
 }
